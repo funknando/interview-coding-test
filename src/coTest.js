b4cc276d926232922d114e31f38fced8232d13e5
funknando <40534208+funknando@users.noreply.github.com>
2018-06-24 02:07:13 -0400

    Update
    Fernando

--- src/coTest.js
+++ src/coTest.js
@@ -44,6 +44,11 @@ class CarInsurance {
             if (this.products[i].price > 0) {
               if (this.products[i].name != 'Mega Coverage') {
                 this.products[i].price = this.products[i].price - 1;
+                 //Fr Inicio//
+                if (this.products[i].name != 'Super Sale') {
+                 this.products[i].price = this.products[i].price - 1; 
+                } 
+                //Fr Fin//
               }
             }
           } else {
